82b80067f698e645f949ab312d8e4ae31aa1a3e9
lithid <long.jeremie@gmail.com>
2012-09-09 18:15:21 -0400

    replaced doublequotes with single

--- share/aoscPy/Globals.py
+++ share/aoscPy/Globals.py
@@ -9,33 +9,33 @@ from Tools import Tools
 class Globals():
 
 	# Resources
-	myMainTitle = "Android Open Source Compiler"
-	myIMGS = "/usr/share/aoscompiler/images"
-	myICON = "%s/aosc-icon.png" % myIMGS
-	myTermWall = "%s/termwall.jpg" % myIMGS
-	myTHEME = "%s/theme/" % myIMGS
-	SyncImg = "%s/sync.png" % myIMGS
-	CompileImg = "%s/compile.png" % myIMGS
-	DeviceImg = "%s/device.png" % myIMGS
-	ClobImg = "%s/clobber.png" % myIMGS
-	myBASH = "/usr/share/aoscompiler/cmcBash"
-	myScreenURL = "https://raw.github.com/lithid/AOSCompiler/master/extras/rom"
-	myTermFont = "Ubuntu Mono 9"
+	myMainTitle = ('Android Open Source Compiler')
+	myIMGS = ('/usr/share/aoscompiler/images')
+	myICON = ('%s/aosc-icon.png') % (myIMGS)
+	myTermWall = ('%s/termwall.jpg') % (myIMGS)
+	myTHEME = ('%s/theme/') % (myIMGS)
+	SyncImg = ('%s/sync.png') % (myIMGS)
+	CompileImg = ('%s/compile.png') % (myIMGS)
+	DeviceImg = ('%s/device.png') % (myIMGS)
+	ClobImg = ('%s/clobber.png') % (myIMGS)
+	myBASH = ('/usr/share/aoscompiler/cmcBash')
+	myScreenURL = ('https://raw.github.com/lithid/AOSCompiler/master/extras/rom')
+	myTermFont = ('Ubuntu Mono 9')
 
 	# Local config junk
-	myHOME = os.environ['HOME']
-	myGIT_CONF = "%s/.gitconfig" % (myHOME)
-	myCONF_DIR = "%s/.aoscompiler" % (myHOME)
-	myCONF = "%s/aoscompiler.conf" % (myCONF_DIR)
-	myCONFIRM = "%s/ask.confim" % (myCONF_DIR)
-	myREPO_CONF = "%s/repo_list" % (myCONF_DIR)
-	myDEF_REPO_PATH = "%s/build" % (myCONF_DIR)
-	askConfirm = "%s/ask.confim" % (myCONF_DIR)
+	myHOME = (os.environ['HOME'])
+	myGIT_CONF = ('%s/.gitconfig') % (myHOME)
+	myCONF_DIR = ('%s/.aoscompiler') % (myHOME)
+	myCONF = ('%s/aoscompiler.conf') % (myCONF_DIR)
+	myCONFIRM = ('%s/ask.confim') % (myCONF_DIR)
+	myREPO_CONF = ('%s/repo_list') % (myCONF_DIR)
+	myDEF_REPO_PATH = ('%s/build') % (myCONF_DIR)
+	askConfirm = ('%s/ask.confim') % (myCONF_DIR)
 
 	# Needed web urls
-	myFORUM_URL = "http://forum.xda-developers.com/showthread.php?t=1415661"
-	myDONATE = "http://forum.xda-developers.com/donatetome.php?u=2709018"
-	myREPO_TOOL_URL = "https://dl-ssl.google.com/dl/googlesource/git-repo/repo"
+	myFORUM_URL = ('http://forum.xda-developers.com/showthread.php?t=1415661')
+	myDONATE = ('http://forum.xda-developers.com/donatetome.php?u=2709018')
+	myREPO_TOOL_URL = ('https://dl-ssl.google.com/dl/googlesource/git-repo/repo')
 
 	PROCESSORS = Tools().processor()
 
@@ -46,20 +46,20 @@ class Globals():
 	mylist = []
 
 	# Info stuff
-	ask_confirm_info = ("<small>By no means what so ever is this software or cyanogenmod responsible for what you do to your phone. "
-			"You are taking the risks, you are choosing to this to your phone. By proceeding you are aware, you are warned. No crying or moaning. This software "
-			"was tested by human beings, not cybogs from your mothers closet. Please keep this in mind when something breaks, or hangs.  If you have an issue "
-			"with this software, please let me know.\n\nBy clicking this ok button, you have given me your soul.\n\nPlay safe.\n\n</small> "
-			"<small><small><b>Note:\n- </b><i>This will not proceed unless you agree.</i></small>\n"
-			"<small><b>-</b><i> Cyanogenmod doesn't consider source builds offical, please keep this in mind if you plan on bug reporting.</i></small></small>\n\n"
-			"Any bugs? Please report them http://github.com/lithid/AOSCompiler/issues\n")
+	ask_confirm_info = ('<small>By no means what so ever is this software or cyanogenmod responsible for what you do to your phone. '
+			'You are taking the risks, you are choosing to this to your phone. By proceeding you are aware, you are warned. No crying or moaning. This software '
+			'was tested by human beings, not cybogs from your mothers closet. Please keep this in mind when something breaks, or hangs.  If you have an issue '
+			'with this software, please let me know.\n\nBy clicking this ok button, you have given me your soul.\n\nPlay safe.\n\n</small> '
+			'<small><small><b>Note:\n- </b><i>This will not proceed unless you agree.</i></small>\n'
+			'<small><b>-</b><i> Cyanogenmod doesn\'t consider source builds offical, please keep this in mind if you plan on bug reporting.</i></small></small>\n\n'
+			'Any bugs? Please report them http://github.com/lithid/AOSCompiler/issues\n')
 
-	about_info = ("The cyanogenmod compiler was written, not to dismiss the need"
-			"to learn the android system, but to release the need consistly remember menial tasks.\n\n"
-			"Please intend to learn the system, contribute back to any upstream.\n\n"
-			"Happy compiling,\n\nCode: Jeremie Long\n"
-			"Any bugs? Please report them\n\n"
-			"http://github.com/lithid/Cmcompiler/issues\n")
+	about_info = ('The cyanogenmod compiler was written, not to dismiss the need'
+			'to learn the android system, but to release the need consistly remember menial tasks.\n\n'
+			'Please intend to learn the system, contribute back to any upstream.\n\n'
+			'Happy compiling,\n\nCode: Jeremie Long\n'
+			'Any bugs? Please report them\n\n'
+			'http://github.com/lithid/Cmcompiler/issues\n')
 
 	MAIN_WIN = gtk.Window(gtk.WINDOW_TOPLEVEL)
 	DEV_BTN = gtk.Button()
